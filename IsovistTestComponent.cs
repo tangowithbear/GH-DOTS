b9bc05deefbf7f7816d63eb26e8649b48658c436
Marina <mmledcb@gmail.com>
2023-05-04 19:16:32 +0200

    Add project files.

---
+++ IsovistTestComponent.cs
@@ -0,0 +1,182 @@
+using Grasshopper;
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+
+namespace IsovistTest {
+    public class IsovistTestComponent : GH_Component {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public IsovistTestComponent()
+          : base("IsovistTest", "ASpi",
+            "Construct an Isovist and evaluate data",
+            "Locus", "Field of view") {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager) {
+            // Use the pManager object to register your input parameters.
+            // You can often supply default values when creating parameters.
+            // All parameters must have the correct access type. If you want 
+            // to import lists or trees of values, modify the ParamAccess flag.
+
+            //pManager.AddPlaneParameter("Plane", "P", "Base plane for spiral", GH_ParamAccess.item, Plane.WorldXY);
+            //pManager.AddNumberParameter("Inner Radius", "R0", "Inner radius for spiral", GH_ParamAccess.item, 1.0);
+            //pManager.AddNumberParameter("Outer Radius", "R1", "Outer radius for spiral", GH_ParamAccess.item, 10.0);
+            //pManager.AddIntegerParameter("Turns", "T", "Number of turns between radii", GH_ParamAccess.item, 10);
+
+            pManager.AddPointParameter("Test Point", "P", "Test point for a spatial unit", GH_ParamAccess.item);
+            pManager.AddPointParameter("All test points", "Ps", "A list of points ", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("View resolution", "VR", "View resolution, 1 for 360, 2 for 720", GH_ParamAccess.item, 1);
+            pManager.AddIntegerParameter("radius", "R", "Lenght of the Ray", GH_ParamAccess.item, 1000);
+            pManager.AddGeometryParameter("Interieor obstacles", "IO", "opaque Building geometry including the exterieor walls", GH_ParamAccess.list);
+            pManager.AddGeometryParameter("Exterior obstacles", "EO", "Opaque Exteriour geometry", GH_ParamAccess.list);
+            pManager.AddGeometryParameter("Bonus view geometry", "B", "Geometry to check the view acces", GH_ParamAccess.item);
+
+            
+           
+
+            // If you want to change properties of certain parameters, 
+            // you can use the pManager instance to access them by index:
+            //pManager[0].Optional = true;
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager) {
+            // Use the pManager object to register your output parameters.
+            // Output parameters do not have default values, but they too must have the correct access type.
+            //pManager.AddCurveParameter("Spiral", "S", "Spiral curve", GH_ParamAccess.item);
+
+            pManager.AddPointParameter("End Points", "EP", "End points of the rays", GH_ParamAccess.list);
+            pManager.AddBrepParameter("Interieor IsoVist", "IIV", "A brep representing interior firld of view for a given test point", GH_ParamAccess.item);
+            pManager.AddBrepParameter("Exterior Isovist", "EIV", "A brep representing a field of view for a given test point", GH_ParamAccess.item);
+            pManager.AddTextParameter("Data", "D", "Properties data containing numerical values", GH_ParamAccess.list);
+
+
+            // Sometimes you want to hide a specific parameter from the Rhino preview.
+            // You can use the HideParameter() method as a quick way:
+            //pManager.HideParameter(0);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA) {
+            // First, we need to retrieve all data from the input parameters.
+            // We'll start by declaring variables and assigning them starting values.
+            //Plane plane = Plane.WorldXY;
+            //double radius0 = 0.0;
+            //double radius1 = 0.0;
+            //int turns = 0;
+
+            Point3d testPoint = Point3d.Unset;
+            List<Point3d> allTestPoints = new List<Point3d>();
+            int resolution = 1;
+            int radius = 1000;
+            List<GeometryBase> interiorObstacles = new List<GeometryBase>();
+            List<GeometryBase> exteriorObstacles = new List<GeometryBase>();
+            GeometryBase bonusViewGeometry = null;
+
+
+            // Then we need to access the input parameters individually. 
+            // When data cannot be extracted from a parameter, we should abort this method.
+
+            //if (!DA.GetData(0, ref plane)) return;
+            //if (!DA.GetData(1, ref radius0)) return;
+            //if (!DA.GetData(2, ref radius1)) return;
+            //if (!DA.GetData(3, ref turns)) return;
+
+            if (!DA.GetData(0, ref testPoint)) return;
+            if (!DA.GetDataList<Point3d>(1, allTestPoints)) return;
+            if (!DA.GetData(2, ref resolution)) return;
+            if (!DA.GetData(3, ref radius)) return;
+            if (!DA.GetDataList<GeometryBase>(4, interiorObstacles)) return;
+            if (!DA.GetDataList<GeometryBase>(5, exteriorObstacles)) return;
+            if (!DA.GetData(6, ref bonusViewGeometry)) return;
+           
+
+            // We should now validate the data and warn the user if invalid data is supplied.
+
+            if (testPoint == Point3d.Unset ) {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "No Test point is provided");
+                return;
+            }
+            if (radius <= 0.0) {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Inner radius must be bigger than zero");
+                return;
+            }
+            if (resolution <= 0 || resolution > 2) {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Resolution parameter must 1 or 2");
+                return;
+            }
+
+
+            // We're set to create the spiral now. To keep the size of the SolveInstance() method small, 
+            // The actual functionality will be in a different method:
+            //Curve spiral = CreateSpiral(plane, radius0, radius1, turns);
+
+
+            List<Point3d> endPoints = ComputeEndPoints(testPoint, radius, resolution);
+            //List<Curve> rays = ComputeRays(endPoints);
+            //Brep interiorIsoVist = 
+            //Brep exteriorIsovist =
+
+            // Finally assign the spiral to the output parameter.
+            // DA.SetData(0, spiral);
+
+            DA.SetDataList(0, endPoints);
+
+        }
+
+        public List<Point3d> ComputeEndPoints(Point3d testPoint, int radius, int resolution) {
+            Plane plane = Plane.WorldXY;
+            Circle c = new Circle(plane, testPoint, radius);
+
+            int N = resolution * 360;
+            List<Point3d> endPoints = new List<Point3d>();
+            double angle = (2 * Math.PI) / N;
+
+            for (int i = 0; i < N; i++) {
+                double t = i * angle;
+                endPoints.Add(c.PointAt(t));
+            }
+
+            return endPoints;
+        }
+
+        /// <summary>
+        /// The Exposure property controls where in the panel a component icon 
+        /// will appear. There are seven possible locations (primary to septenary), 
+        /// each of which can be combined with the GH_Exposure.obscure flag, which 
+        /// ensures the component will only be visible on panel dropdowns.
+        /// </summary>
+        public override GH_Exposure Exposure => GH_Exposure.primary;
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// You can add image files to your project resources and access them like this:
+        /// return Resources.IconForThisComponent;
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => null;
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("8984EDFF-6F6C-4384-A818-D5316B589D88");
+    }
+}
---
+++ IsovistTestInfo.cs
@@ -0,0 +1,24 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace IsovistTest {
+    public class IsovistTestInfo : GH_AssemblyInfo {
+        public override string Name => "IsovistTest";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("88D89299-DE92-4BB6-AB8E-6CD4E17A0125");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "Marina";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
